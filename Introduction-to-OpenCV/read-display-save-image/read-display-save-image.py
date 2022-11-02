e2d7e2e7bf72837ae27020c5cfa317dbaeb2f563
İlker YILMAZ <53461108+ilkerylmaz@users.noreply.github.com>
2022-11-02 17:26:54 +0300

    image read-display-save

---
+++ Introduction-to-OpenCV/read-display-save-image/read-display-save-image.py
@@ -0,0 +1,8 @@
+import cv2
+
+img = cv2.imread('ortakoy.jpg',1) # 0 for grayscale, 1 for color
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image', img)
+cv2.imwrite('ortakoy_copy.jpg', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OpenCV-tutorials
  
+<h4>Read - Display - Save Image</h4>
+![img.png](img.png)
